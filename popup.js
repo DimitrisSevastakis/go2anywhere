5a2fd1b3f6615041f212f6131ebef5c81ef3b3d9
diwora <sell_my_soul_2_roll@hotmail.com>
2013-12-13 02:44:09 +0100

    fixes and new features
    small efficiency improvements
    before opening new tab with selected bookmark, checks if similar tab
    already exists, if so brings that tab to the foreground, else opens new
    tab.
    If the current tab is a new tab page, opens the bookmark in that tab
    instead of opening new one

--- popup.js
+++ popup.js
@@ -8,14 +8,19 @@
 // Traverse the bookmark tree, and print the folder and nodes.
 function dumpBookmarks(query) {
     chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
-        $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
+       dumpTreeNodes(bookmarkTreeNodes, query, '/');
     });
 }
-function dumpTreeNodes(bookmarkNodes, query) {
+function dumpTreeNodes(bookmarkNodes, query, parent) {
     var i;
     for (i = 0; i < bookmarkNodes.length; i++) {
-        dumpNode(bookmarkNodes[i], query);
+        dumpNode(bookmarkNodes[i], query, parent);
     }
+}
+String.prototype.replaceAt = function (i, char) {
+
+    return this.substr(0, i) + char + this.substr(i + 1);
+
 }
 
 //fuzzy search function
@@ -32,11 +37,12 @@ function isMatch(str, q){
     return index;
 }
 
-function dumpNode(bookmarkNode, query) {
+function dumpNode(bookmarkNode, query, parent) {
+    var ind, i, temp;
 
+    temp = String(bookmarkNode.title);
     if (query && !bookmarkNode.children) {
         var predicate;
-
         //if '@' search this element's title of exact match
         if(query.indexOf('@') == 0){
             q = query.substring(1);
@@ -49,7 +55,8 @@ function dumpNode(bookmarkNode, query) {
         }
         //else search this element's title for fuzzy match and url for exact match
         else{
-            predicate = (isMatch(String(bookmarkNode.title.toLowerCase()), query.toLowerCase())==-1 && String(bookmarkNode.url.toLowerCase()).indexOf(query.toLowerCase()) == -1);
+            ind = isMatch(String(bookmarkNode.title.toLowerCase()), query.toLowerCase());
+            predicate = (ind==-1 && String(bookmarkNode.url.toLowerCase()).indexOf(query.toLowerCase()) == -1);            
         }
 
         //if there is no match return empty element
@@ -57,39 +64,71 @@ function dumpNode(bookmarkNode, query) {
             return $('<span></span>');
         }
     }
-var anchor = $('<p>');
-anchor.text(bookmarkNode.title);
 
-/*
-* When clicking on a bookmark in the extension, a new tab is fired with
-* the bookmark url.
-*/
+    var anchor = $('<p>');
+    anchor.html(temp);
 
     var span = $('<span>');
-    var edit = bookmarkNode.children ? $('<table><tr><td>Name</td><td>' +
-        '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
-        '</td></tr></table>') : $('<input>');
-
     span.append(anchor);
 
     var li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
 
     if (bookmarkNode.children && bookmarkNode.children.length > 0) {
-        dumpTreeNodes(bookmarkNode.children, query);
+        var par = (!String(bookmarkNode.title)) ? parent : parent+'/' + bookmarkNode.title
+        dumpTreeNodes(bookmarkNode.children, query, par.replace('//', '/'));
     }
-    li.append('<p id="urladdr">'+bookmarkNode.url +'</p>');
+    li.append('<p class="urladdr">'+bookmarkNode.url +'<span class="bookmark_path">('+parent +')</span></p>');
     if(!bookmarkNode.children) {
         li.attr('href', bookmarkNode.url);
         li.click(function() {
-            chrome.tabs.create({url: bookmarkNode.url});
+            $('li[data-selected=true]').css('background-color', '#293134').attr('data-selected', false);
+            li.css('background-color', '#3b4a50').attr('data-selected', true).attr('data-ind', 0);
+            handleSelect();
         });
     }
 
     if(!bookmarkNode.children) $('#bookmarks').append(li);
     
-    $('li').css('background-color', '#293134').attr('data-selected', false);
+    $('li[data-selected=true]').css('background-color', '#293134').attr('data-selected', false);
     $('li:first').css('background-color', '#3b4a50').attr('data-selected', true).attr('data-ind', 0);
 
+}
+
+
+//handles a selection, where it is done by mouse click or 'enter' button
+function handleSelect(){
+    s = $('li[data-selected=true]');
+    //get all open tabs
+    tabs = chrome.tabs.query({},function(tabs){
+        var j;
+        var tab_exists = false;
+        //go through all the open tabs
+        for(j=0; j<tabs.length; j++){
+            //if a tab already exists with this url bring that tab forth, along with its window
+            if(tabs[j].url.replace('http://', '') == s.attr('href').replace('http://', '')){
+                //bring window forth
+                chrome.windows.update(tabs[j].windowId,{focused: true});
+                //bring tab forth
+                chrome.tabs.update(tabs[j].id, {active: true});
+                tab_exists = true;
+                break;
+            }   
+        }
+
+        //handle opening of new tab
+        if(!tab_exists){
+            //get the currently focused tab
+            chrome.tabs.getSelected(function(tab){
+                if(tab.title == 'New Tab'){
+                //if it is the 'New Tab' page open the url in the current tab
+                    chrome.tabs.update(tab.id, {url: s.attr('href')});
+                }else{
+                //else open new tab
+                    chrome.tabs.create({url: s.attr('href')});
+                }
+            });
+        }
+    });
 }
 
 //when the extension window is loaded do:
@@ -109,21 +148,19 @@ document.addEventListener('DOMContentLoaded', function () {
             switch(event.which){
                 case 13:
                     //case enter open selected bookmark
-                    chrome.tabs.create({url: s.attr('href')});
+                    handleSelect();
                     break;
                 case 38:
                     //case up-arrow select previous bookmark
-                    console.log('up');
                     if(parseInt(s.attr('data-ind'))>0){
-                        $('li').css('background-color', '#293134').attr('data-selected', false);
+                        s.css('background-color', '#293134').attr('data-selected', false);
                         $('li').eq(parseInt(s.attr('data-ind'))-1).css('background-color', '#3b4a50').attr('data-selected', true).attr('data-ind', parseInt(s.attr('data-ind'))-1);
                     }
                     break;
                 case 40:
                     //case up-arrow select previous bookmark
-                    console.log('down');
                     if(parseInt(s.attr('data-ind'))<$('li').length){
-                        $('li').css('background-color', '#293134').attr('data-selected', false);
+                        s.css('background-color', '#293134').attr('data-selected', false);
                         $('li').eq(parseInt(s.attr('data-ind'))+1).css('background-color', '#3b4a50').attr('data-selected', true).attr('data-ind', parseInt(s.attr('data-ind'))+1);
                     }
             }
